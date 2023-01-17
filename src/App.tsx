596cc7391050fda5b4f8fe81a3f067c4b65b9565
Vladyslav Suhyh <vcyx@ukr.net>
2023-01-17 14:07:53 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {useMetamask} from "./hooks/useMetamask";
+import {ethers, Wallet} from "ethers";
+
+// BDOD
+const PRIVATE_KEY = 'd08b5a0794ea26c523a96c3c9a9a5a9eaad43056a5ef593a24eb14c58fc8c049'
 
 function App() {
+  const {connect, accounts, provider } = useMetamask()
+    const [addressToSend, setAddressToSend] = useState<string>('')
+    const [signer, setSigner] = useState<Wallet>()
+
+    useEffect(()=>{
+        (async ()=> await connect())()
+    }, [])
+
+    useEffect(()=>{
+        setSigner(new ethers.Wallet(PRIVATE_KEY, provider!));
+    }, [provider])
+
+    const onSend = async () =>{
+        const resp = await signer!.sendTransaction({
+            to: addressToSend,
+            value: ethers.utils.parseEther("0.0001")
+        })
+
+        console.log('resp', resp)
+
+        console.log('Done')
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App">
+        <header className="App-header">
+            Choose BDOD wallet, Goerli chain!
+
+            <pre>
+                {JSON.stringify(accounts)}
+            </pre>
+
+            Address to send:
+            <input value={addressToSend} onChange={(e)=>setAddressToSend(e.target.value)}/>
+            0.0001 ETH
+
+            <button onClick={onSend}>Send</button>
+
+            Check here after:
+            https://goerli.etherscan.io/address/0xB7C1044A6dBd372105fb7B12738e0Dd1971eBD0D
       </header>
     </div>
   );
